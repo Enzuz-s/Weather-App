c20581679f822a1d59daa6e15b9a3f7f0c31dfe8
Enzuz-s <EnhzuzHD@gmail.com>
2022-05-31 13:47:11 +0200

    build

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
 import setuptools
 
-with open("README.MD", "r", encoding="utf-8") as fh:
-    long_description = fh.read())
+with open("README.md", "r", encoding="utf-8") as a:
+    long_description = a.read()
 
 setuptools.setup(
-    name="Weather-app-Enzuz-s"
-    version="1.0"
+    name="Weather-app-Enzuz-s",
+    version="1.0",
     author="Enzuz-s",
     description="A small Weather App",
     url="https://github.com/Enzuz-s/Weather-App",
+    long_description=long_description,
     project_urls={
         "Bug Tracker": "https://github.com/Enzuz-s/Weather-App/issues",
     },
 
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: MIT License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir = {"": "src"},
-    packages = setuptools.find_packages(where="src"),
-    python_requires = ">3.10",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">3.10",
 )
